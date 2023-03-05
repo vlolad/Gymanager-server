f8d07bf4daee000cd29832c6e2f3de53b4875b77
vlolad <vlad19.99@yandex.ru>
2023-03-05 22:39:16 +0300

    test simple architecture

---
+++ main-server/src/main/java/ru/gymanager/server/model/SimpleMessage.java
@@ -0,0 +1,10 @@
+package ru.gymanager.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SimpleMessage {
+    private String message;
+}
